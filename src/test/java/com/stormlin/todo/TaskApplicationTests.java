6ca225ebfd6ef44e173867d71038b0cf7567f6fd
lin-jinting <lin-jinting@outlook.com>
2021-08-23 11:00:00 +0800

    initial commit

---
+++ src/test/java/com/stormlin/todo/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stormlin.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
